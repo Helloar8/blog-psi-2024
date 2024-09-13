a5e75da9fdd0e0d213220743d10be97aaae896e3
Helloar <novaeheloar@gmail.com>
2024-09-13 10:21:48 -0300

    commit final do dia 13

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('post/<int:id', views.postagem, name='postagem'),
 ]
+ 
--- blog/views.py
+++ blog/views.py
@@ -9,3 +9,8 @@ def index(request):
         "posts" : posts,
     }
     return render(request, "index.html", context)
+
+
+def postagem(request, id):
+    posts = Post.objects.get
+    return render(request, "post.html")
